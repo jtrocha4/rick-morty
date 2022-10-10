da1a87e102aea5be1c5d565e139ee23070528fba
Jaime Trocha <jtrocha04@gmail.com>
2022-10-10 17:13:36 -0500

    Informacion sobre la serie

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   return (
     <div>
       <h1>Rick & Morty Wiki</h1>
+      <br></br>
+      <h3>Descripcion</h3>
+      <p>Después de haber estado desaparecido durante casi 20 años, Rick Sánchez llega de imprevisto a la puerta de la casa de su hija Beth y se va a vivir con ella y su familia utilizando el garaje como su laboratorio personal.</p>
+
+      <p><b>Cantidad de temporadas: </b>6</p>
+      <p><b>Cantidad de episodios: </b>55</p>
+      <p><b>Creadores del programa: </b>Justin Roiland, Dan Harmon</p>
+
       <PeticionApi></PeticionApi>
     </div>
   );
